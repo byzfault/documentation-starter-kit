c162d67215b082b566b16d11ab79645f6f7f3810
James Aindow <everycloud4@proton.me>
2025-10-13 12:39:18 +0100

    Add GTM and fix some minor text changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,14 +3,44 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>James Aindow — Technical Writer</span>,
+
   useNextSeoProps() {
     return {
       titleTemplate: '%s'
     }
   },
+
   footer: {
     text: 'Bridging the gap between technical and non technical',
   },
+
+  feedback: {
+    content: null  // This removes "Question? Give us feedback"
+  },
+
+  editLink: {
+    component: null  // This removes "Edit this page"
+  },
+
+  // ---- Add Google Analytics here ----
+  head: (
+    <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-B5TMY3QM7J"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B5TMY3QM7J');
+          `,
+        }}
+      />
+    </>
+  ),
 }
 
 export default config
